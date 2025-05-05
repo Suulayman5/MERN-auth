6ba9ae9cdc9c6f9ed604f4bdebdc9aba5235c20f
suulayman5 <ajahsuleiman@gmail.com>
2025-05-05 16:24:20 +0100

    update cookie settings in generateTokenAndSetCookies to use secure and sameSite: none

--- backend/utils/generateTokenAndSetCookies.js
+++ backend/utils/generateTokenAndSetCookies.js
@@ -10,8 +10,9 @@ export const generateTokenAndSetCookies = (res, userId) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    secure: true,    
+    sameSite: 'none',
+     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
   return token;
