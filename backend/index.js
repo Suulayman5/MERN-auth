ca3a8ca16b58808d5de3b0b483f8fd245000a4b8
suulayman5 <ajahsuleiman@gmail.com>
2025-05-09 15:05:28 +0100

    restore CORS middleware and re-enable credentials support

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-// import cors from "cors";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 
@@ -14,10 +14,12 @@ dotenv.config();
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
-const __dirname = path.resolve(); 
+// const __dirname = path.resolve(); 
  
-//   app.use(cors({ credentials: true }));
-
+app.use(cors({
+	origin: true, // or specify origin explicitly e.g. "https://your-frontend.vercel.app"
+	credentials: true,
+  }));
   
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
@@ -26,13 +28,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/item", itemRoutes);
 app.use("/api/categories", categoryRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 app.listen(PORT, () => {
 	connectDB();
